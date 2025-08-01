c7dbda2e8965d06a2460017bb1e4445554c89c88
FardeenAttar <fardeenisa@gmail.com>
2025-08-01 19:50:22 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 // // const API_BASE_URL = "http://localhost:313";
-const API_BASE_URL = "http://be.coverbazar.shop/";
+const API_BASE_URL = "http://backend-lb-907916041.us-east-1.elb.amazonaws.com";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
